e0ca0158854153553969b072f73bd741fc7596f5
Ramesh M S <msramesh2k1@gmail.com>
2021-12-20 16:48:24 +0530

    feat : Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trendz/Controllers/facebook_login_controller.dart';
 import 'package:trendz/Controllers/google_login_controller.dart';
 
 import 'Views/splash_screen.dart';
@@ -17,10 +18,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider.value(value: GoogleSignin)
-    ]);
-    MaterialApp(
-        title: 'Trendz', theme: ThemeData(), home: const SplashScreen());
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(value: GoogleSigninController()),
+          ChangeNotifierProvider.value(value: FacebookLoginController())
+        ],
+        child: MaterialApp(
+            title: 'Trendz', theme: ThemeData(), home: const SplashScreen()));
   }
 }
