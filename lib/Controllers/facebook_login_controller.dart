6a2557478f72e3a721bfcc229e4f8c80f9b16794
Ramesh M S <msramesh2k1@gmail.com>
2021-12-21 09:15:16 +0530

    feat : Google fb auth

--- lib/Controllers/facebook_login_controller.dart
+++ lib/Controllers/facebook_login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:provider/provider.dart';
@@ -9,15 +10,19 @@ class FacebookLoginController extends ChangeNotifier {
 
     if (result.status == LoginStatus.success) {
       final requestdata =
-          await FacebookAuth.i.getUserData(fields: "name,email,picture,id");
+      await FacebookAuth.i.getUserData(fields: "name,email,picture,id");
 
       userdata = requestdata;
 
       final AccessToken accessToken = result.accessToken!;
+
+      final OAuthCredential credential = FacebookAuthProvider.credential(accessToken.token);
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(credential);
+      
       notifyListeners();
       print(requestdata);
 
-      print("Logged IN");
     } else {
       print(result.status);
       print(result.message);
