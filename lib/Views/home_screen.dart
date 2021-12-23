6450ea726ec122def7b130a061f68fe1c07bebf8
Ramesh M S <msramesh2k1@gmail.com>
2021-12-23 10:38:51 +0530

    feat : Google fb auth

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trendz/Controllers/logincontroller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +13,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                LoginController().signout();
+              },
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.black,
+              ))
+        ],
+        title: Text(
+          "TREND - Z",
+          style: GoogleFonts.josefinSans(
+            textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 18,
+                letterSpacing: 2),
+          ),
+        ),
+        backgroundColor: Colors.blue[100]!.withOpacity(0.2),
+        elevation: 0,
+      ),
     );
   }
 }
